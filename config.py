58c80d47e85d02d72fabe99952a211304e52afb7
kelvinhovinho <kelvinadamba@hotmail.com>
2021-03-01 01:29:43 +0300

    added the news api key and the secret word

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&apiKey='
+    ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DEBUG = True
+
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+   
+   
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,8 @@
+export NEWS_API_KEY="33c2f719acc64b268ba3a90357a35a01"
+export SECRET_KEY="kelvinnews"
+
+python3.6 manage.py server
+
+
+
+
